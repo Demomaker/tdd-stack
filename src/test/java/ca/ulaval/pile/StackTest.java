d18d06ffeb519953bb0d1e863982eb43181100b8
Felix-Antoine Bourbonnais <fbourbonnais@elapsetech.com>
2021-09-14 16:54:06 -0400

    ...

--- src/test/java/ca/ulaval/pile/StackTest.java
+++ src/test/java/ca/ulaval/pile/StackTest.java
@@ -6,75 +6,39 @@ import org.junit.Test;
 
 public class StackTest {
   int AN_ELEMENT = 5;
+  int FIRST_PUSHED = 10;
+  int SECOND_PUSHED = 14;
+
+  Stack pile = new Stack();
 
   @Test
   public void isInitiallyEmpty() {
-    Stack pile = new Stack();
     assertTrue(pile.isEmpty());
   }
-  
+
   @Test
   public void whenPush_thenAddTheElement() {
-    Stack pile = new Stack();
     pile.push(AN_ELEMENT);
     assertFalse(pile.isEmpty());
   }
 
-
   @Test
   public void givenOneElement_whenPopping_thenElementIsRemoved() {
-    Stack pile = new Stack();
     pile.push(AN_ELEMENT);
-
     pile.pop();
-
     assertTrue(pile.isEmpty());
   }
-  
-  @Test
-  public void givenOneElement_whenPopping_thenReturnTheElement() {
-    int THE_ELEMENT = 5;
-    Stack pile = new Stack();
-    pile.push(THE_ELEMENT);
-    
-    int elementReturned = pile.pop();
-    
-    assertEquals(THE_ELEMENT, elementReturned);
-  }
 
   @Test
-  public void givenMultipleElements_whenPopping_thenReturnLastElementPushed() {
-    int THE_ELEMENT = 10;
-    Stack pile = new Stack();
-    pile.push(AN_ELEMENT);
-    pile.push(THE_ELEMENT);
+  public void givenSomeElements_whenPopping_thenReturnThemInReverseOrder() {
+    pile.push(FIRST_PUSHED);
+    pile.push(SECOND_PUSHED);
 
-    int elementReturned = pile.pop();
+    int firstPopped = pile.pop();
+    int secondPopped = pile.pop();
 
-    assertEquals(THE_ELEMENT, elementReturned);
+    assertEquals(SECOND_PUSHED, firstPopped);
+    assertEquals(FIRST_PUSHED, secondPopped);
   }
 
-  @Test
-  public void givenTwoElements_whenPoppingTwice_thenReturnFirstElementPushed() {
-    int THE_ELEMENT = 10;
-    Stack pile = new Stack();
-    pile.push(THE_ELEMENT);
-    pile.push(AN_ELEMENT);
-
-    pile.pop();
-    int elementReturned = pile.pop();
-
-    assertEquals(THE_ELEMENT, elementReturned);
-  }
 }
-
-
-
-
-
-
-
-
-
-
-
